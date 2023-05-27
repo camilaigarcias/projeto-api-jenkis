c75e1d61fc51d054a4349f11a05b23991e3bad8e
camilaigarcias <89035348+camilaigarcias@users.noreply.github.com>
2023-05-26 21:56:25 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -15,6 +15,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'http://localhost:3500/',
+    baseUrl: 'http://localhost:3000/',
   },
 })
